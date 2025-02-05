81632a85e9cc8b5f5887c8328dbc19be6f8d7ddf
SchubertChris <schubert_chris@ocketmail.com>
2025-02-05 15:14:09 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './assets/components/jsx/Navbar'
-import KurzEinleitung from './assets/components/jsx/KurzEinleitung'
-import About from './assets/components/jsx/About.jsx'
-import Kontakt from './assets/components/jsx/Kontakt.jsx'
-import Galerie from './assets/components/jsx/Galerie.jsx'
+import Navbar from './components/jsx/Navbar'
+import KurzEinleitung from './components/jsx/KurzEinleitung'
+import About from './components/jsx/About.jsx'
+import Kontakt from './components/jsx/Kontakt.jsx'
+import Galerie from './components/jsx/Galerie.jsx'
 
 export default function App() {
 
